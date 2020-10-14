0c401cb1dee88206ea9f019ad6e2c90be0d2b188
Weerayooth Manawanich <weerayooth.ma@gmail.com>
2020-10-14 17:05:42 +0700

    first commit

---
+++ src/pages/ContactPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import logo from "./logo.svg";
+import "../App.css";
+import Calculator from "../components/Calculator";
+
+function ContactPage() {
+    return <div className="App" style={{ width: '100vw', height: '100vh', backgroundColor: '#222222', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+        <Calculator />
+    </div>;
+}
+
+export default ContactPage
---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TodoListHook from '../components/TodoListHook'
+
+function HomePage() {
+    return (
+        <div >
+            <TodoListHook />
+        </div>
+    )
+}
+
+export default HomePage
